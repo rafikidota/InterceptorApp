2d2caa68613c090b2a8590e6f4c49315214b6bb9
rafikidota <lesmes240698@gmail.com>
2022-09-17 23:48:54 -0400

    throw => return

--- src/app/interceptors/interceptor.service.ts
+++ src/app/interceptors/interceptor.service.ts
@@ -10,6 +10,7 @@ export class InterceptorService implements HttpInterceptor {
   constructor() { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
     const headers = new HttpHeaders({
       'x-token': 'ABC123456789XYZ'
     });
@@ -18,10 +19,10 @@ export class InterceptorService implements HttpInterceptor {
       headers
     });
 
-    throw next.handle(req_clone)
-              .pipe(
-                catchError(this.handleError)
-              );
+    return next.handle(req_clone).pipe(
+      catchError(this.handleError)
+    );
+
   }
 
   handleError(error: HttpErrorResponse) {
